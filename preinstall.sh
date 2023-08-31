8d06f17fb47ea6e17c3f02c3792e0bd2669760b1
Nodes | BullRancho <133574162+NodesLLS@users.noreply.github.com>
2023-08-31 22:09:05 +0300

    Add files via upload

---
+++ preinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/NodesLLS/Node_tools/main/logo.sh | bash
+sudo apt update
+sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler -y
