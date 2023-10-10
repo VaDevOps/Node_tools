81b74bb47509ebab74018ac9335f29817de4bd8b
VaDevOps <109411188+VaDevOps@users.noreply.github.com>
2023-10-10 19:56:30 +0300

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/NodesLLS/Node_tools/main/logo.sh | bash
 sudo apt update
 sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler -y
